10013c6e152f8a620acbb26c3c7dc01d2e064b19
jmkim <isugar150@gmail.com>
2025-01-26 14:09:40 +0900

    ✨ init project

---
+++ src/main/java/com/namejm/kafkastudy/component/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.namejm.kafkastudy.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+    public void consume(String message) {
+        log.info("Received message: {}", message);
+        // 메시지 처리 로직을 작성합니다.
+    }
+}
